2b745b387017722eaad1e751c34fc3dd9c852c4d
David Ryan <davidryan1998@hotmail.com>
2017-10-27 13:27:44 +0100

    All method added to queryHelper. Need to pass in the collection name e.g. pubs as a parameter.

--- server/db/query_helper.js
+++ server/db/query_helper.js
@@ -2,8 +2,16 @@ var MongoClient = require('mongodb').MongoClient
 
 var queryHelper = {}
 
-queryHelper.testFunction = function(){
-  
+queryHelper.url = "mongodb://localhost:27017/local_information"
+
+// Example call: queryHelper.all('pubs', function(){})
+queryHelper.all = function(theCollectionName, onQueryFinished){
+  MongoClient.connect(this.url, function(err, db){
+    var theCollection = db.collection(theCollectionName)
+    theCollection.find().toArray(function(err, docs){
+      onQueryFinished(docs)
+    })
+  })
 }
 
 module.exports = queryHelper

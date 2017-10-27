1f5ecc72888290aa28d5883bb91bce8a608d6209
David Ryan <davidryan1998@hotmail.com>
2017-10-27 11:59:29 +0100

    Folder structure finished. App needs setting up.

---
+++ client/src/models/dummy_model.js
@@ -0,0 +1,10 @@
+var DummyModel = function(attibutes) {
+  // attributes is an object {a:"b", c:"d"}
+  // this.theAttribute = attributes.attribute
+}
+
+DummyModel.prototype.methodName = function() {
+  return null
+}
+
+module.exports = DummyModel
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: __dirname + "/app.js",
+  entry: __dirname + "/src/app.js",
   output: {
     filename: "bundle.js",
     path: __dirname + "/build"

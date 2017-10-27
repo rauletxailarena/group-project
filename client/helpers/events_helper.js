308eb694e95c4fc08f3df033597838a9f358ffc2
Allyson Wilson <allysonmacwilson@gmail.com>
2017-10-27 21:17:52 +0100

    added API key to script tag

--- client/helpers/events_helper.js
+++ client/helpers/events_helper.js
@@ -1,8 +1,8 @@
 var eventsHelper = {
 
-  getRequest: function (, callback) {
+  getRequest: function (url, callback) {
     var url =
-    console.log("url ", url);
+
     var xhr = new XMLHttpRequest()
     xhr.open('GET', url)
 
@@ -15,24 +15,6 @@ var eventsHelper = {
 
     xhr.send()
   }
-  // postRequest: function (url, callback, payload) {
-  //   var xhr = new XMLHttpRequest()
-  //   xhr.open('POST', url)
-  //
-  //   xhr.addEventListener('load', function () {
-  //     if (xhr.status !== 200) return
-  //     var jsonString = xhr.responseText
-  //     var data = JSON.parse(jsonString)
-  //     callback(data)
-  //   })
-  //
-  //   xhr.setRequestHeader('Content-Type', 'application/json')
-  //
-  //   var jsonString = JSON.stringify(payload)
-  //
-  //   xhr.send(jsonString)
-  //
-  // }
-}
+
 
 module.exports = eventsHelper

1b10e0b43d5ba643f5fa34c0ec72319f729baf5b
Allyson Wilson <allysonmacwilson@gmail.com>
2017-10-27 15:34:10 +0100

    add map tested

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,12 @@
-var mapWrapper = require ('/../helpers/mapWrapper')
+var mapWrapper = require ('../helpers/mapWrapper')
 
 
-var mapContainer = document.getElementById("map");
-var map = new mapWrapper(mapContainer ,var center = {
-  lat: 51.5074, lng: -0.1278}, 5 );
+
+
+  window.addEventListener('load', function(){
+    var mapContainer = document.getElementById("map");
+    console.log(mapContainer);
+    var map = new mapWrapper(mapContainer,
+      {lat: 51.5074, lng: -0.1278}, 5 );
+    map.addClickEvent();
+  });

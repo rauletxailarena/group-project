f4f2c9d78ae2198996816bf586f499cc73bc6a76
Raul Ruiz <rauletxailarena@gmail.com>
2017-10-30 22:43:04 +0000

    Elements now hide and display accodring to the stage

--- client/src/views/display_welcome_view.js
+++ client/src/views/display_welcome_view.js
@@ -1,15 +1,25 @@
 var displayWelcomeViews = {
   removeWelcome: function(){
-    button = document.getElementById("welcome-input-button")
-    input = document.getElementById("welcome-input")
-    welcomeText = document.getElementById("welcome-text-label")
-    button.style.display = "none";
-    input.style.display = "none";
-    welcomeText.style.display = "none"
+    // button = document.getElementById("welcome-input-button")
+    // input = document.getElementById("welcome-input")
+    // welcomeText = document.getElementById("welcome-text-label")
+    // button.style.display = "none";
+    // input.style.display = "none";
+    // welcomeText.style.display = "none"
+    var elementsToHide = document.getElementsByClassName("hide-after-submit")
+    for (var element of elementsToHide){
+      element.style.display = "none";
+    }
+    var elementsToDisplay = document.getElementsByClassName("hide-at-welcome")
+    console.log("Displaying previously hidden elements", elementsToDisplay);
+    for (var element of elementsToDisplay){
+      element.style.display = "flex";
+
+    }
   },
   renderWelcomeScreen: function(){
-    var elementsToRemove = document.getElementsByClassName("hide-at-welcome")
-    for (var element of elementsToRemove){
+    var elementsToHide = document.getElementsByClassName("hide-at-welcome")
+    for (var element of elementsToHide){
       element.style.display = "none";
     }
   }

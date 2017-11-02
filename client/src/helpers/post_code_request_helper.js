a03362b12489318ddce5cd25abed656f6a467eba
David Ryan <davidryan1998@hotmail.com>
2017-11-02 08:28:01 +0000

    Postcode helper now uses request helper

--- client/src/helpers/post_code_request_helper.js
+++ client/src/helpers/post_code_request_helper.js
@@ -1,38 +1,12 @@
-var postCodeRequestHelper = {
-  temp_url: "http://api.postcodes.io/postcodes/",
-  getRequest: function (postcode, callback) {
-    var url = this.temp_url + postcode
-    // console.log("url ", url);
-    var xhr = new XMLHttpRequest()
-    xhr.open('GET', url)
+var requestHelper = require('./request_helper.js')
 
-    xhr.addEventListener('load', function () {
-      // console.log(xhr);
-      var jsonString = xhr.responseText
-      var data = JSON.parse(jsonString)
+var postCodeRequestHelper = {
+  getRequest: function (postcode, callback) {
+    var url = "http://api.postcodes.io/postcodes/" + postcode
+    requestHelper.getRequest(url, function(data){
       callback(data)
     })
-
-    xhr.send()
   }
-  // postRequest: function (url, callback, payload) {
-  //   var xhr = new XMLHttpRequest()
-  //   xhr.open('POST', url)
-  //
-  //   xhr.addEventListener('load', function () {
-  //     if (xhr.status !== 200) return
-  //     var jsonString = xhr.responseText
-  //     var data = JSON.parse(jsonString)
-  //     callback(data)
-  //   })
-  //
-  //   xhr.setRequestHeader('Content-Type', 'application/json')
-  //
-  //   var jsonString = JSON.stringify(payload)
-  //
-  //   xhr.send(jsonString)
-  //
-  // }
 }
 
 module.exports = postCodeRequestHelper

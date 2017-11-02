67cd6c5d9fa55edc38c9728a136838ab3af064a7
David Ryan <davidryan1998@hotmail.com>
2017-11-02 10:03:57 +0000

    Remove button now works on saved events

--- client/src/views/display_events.js
+++ client/src/views/display_events.js
@@ -1,9 +1,9 @@
 var mapController = require("../helpers/map_controller.js")
 var requestHelper = require("../helpers/request_helper.js")
-var Event = require("../models/night_out_event.js")
+var NightOutEvent = require("../models/night_out_event.js")
 var displayEvents = {
 
-  renderMarker: function(event) {
+  renderMarker: function(nightOutEvent) {
     var colourMarker = "public/markers/a_blue_events_marker.png"
     // create info window for the element
     var container = document.createElement("div")
@@ -13,11 +13,11 @@ var displayEvents = {
     var postcodeHTML = document.createElement("p")
     // var tagsHTML = document.createElement("p")
 
-    nameHTML.textContent = event.name;
-    descriptionHTML.textContent = event.description;
-    venueHTML.textContent = event.venue;
-    postcodeHTML.textContent = event.postcode;
-    // tagsHTML.textContent = event.tags;
+    nameHTML.textContent = nightOutEvent.name;
+    descriptionHTML.textContent = nightOutEvent.description;
+    venueHTML.textContent = nightOutEvent.venue;
+    postcodeHTML.textContent = nightOutEvent.postcode;
+    // tagsHTML.textContent = nightOutEvent.tags;
 
     container.appendChild(nameHTML);
     container.appendChild(descriptionHTML);
@@ -25,28 +25,42 @@ var displayEvents = {
     // container.appendChild(tagsHTML);
     container.appendChild(postcodeHTML);
 
-    if (event.canAdd()) {
+    if (nightOutEvent.canAdd()) {
       var saveButton = document.createElement("button")
       saveButton.addEventListener("click", function(eventObject){
         var url = "http://localhost:3000/api/locations"
         var callback = function(postResponseData){
-          console.log("Saved event, with response:", postResponseData)
+          console.log("Saved Night Out Event, with response:", postResponseData)
         }
-        var payload = event
+        var payload = nightOutEvent
         requestHelper.postRequest(url, callback, payload)
       })
       saveButton.textContent = "Add to my plan";
       container.appendChild(saveButton);
     }
 
-    mapController.addColourMarker(event, colourMarker, container)
+    if (nightOutEvent.canRemove()) {
+      var mongoId = nightOutEvent["_id"]
+      var deleteButton = document.createElement("button")
+      deleteButton.addEventListener("click", function(eventObject){
+        var url = "http://localhost:3000/api/locations" + "/" + mongoId
+        var callback = function(postResponseData){
+          console.log("Deleted Night Out Event, with response:", postResponseData)
+        }
+        requestHelper.deleteRequest(url, callback)
+      })
+      deleteButton.textContent = "Remove from my plan";
+      container.appendChild(deleteButton)
+    }
+
+    mapController.addColourMarker(nightOutEvent, colourMarker, container)
   },
 
   renderMarkers: function(eventsList){
     eventsList.forEach(function(apiEvent){
-      var event = new Event(apiEvent)
-      this.renderMarker(event)
-      // console.log(event)
+      var nightOutEvent = new NightOutEvent(apiEvent)
+      this.renderMarker(nightOutEvent)
+      // console.log(nightOutEvent)
     }.bind(this));
   }
 
